def24635323787619450aa71d255c531cd13a73a
sonal-ssj <60361757+sonal-ssj@users.noreply.github.com>
2026-06-17 23:59:50 +0000

    Deploying to gh-pages from @ sonal-ssj/sonal-ssj.github.io@71de8ef91d96de333b778bd95092bc926af93933 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,25 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "post-what-breaks-this-system-and-why-and-how-can-i-fix-it",
+        
+          title: "What Breaks This System, and Why, and How Can I Fix It?",
+        
+        description: "Ten years, four domains, one question.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/what-breaks-this-system/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
