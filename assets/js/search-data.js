77fd71a840b7993518b93c39f3104ec4bac76734
sonal-ssj <60361757+sonal-ssj@users.noreply.github.com>
2026-06-18 00:53:50 +0000

    Deploying to gh-pages from @ sonal-ssj/sonal-ssj.github.io@3a7871e4a1055fd327426d42e59683ec48ceb7c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-blog",
-          title: "blog",
+        },{id: "nav-thoughts",
+          title: "thoughts",
           description: "",
           section: "Navigation",
           handler: () => {
